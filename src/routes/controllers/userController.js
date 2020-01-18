8abb082e213a1f3da914fa9ff7b684157392a784
NhanVo <voasd123@gmail.com>
2020-01-18 16:37:29 +0700

    WMS-32 modifying user controller

--- src/routes/controllers/userController.js
+++ src/routes/controllers/userController.js
@@ -5,23 +5,23 @@ const {
 
 const User = require('../../db/models/user')(db, Sequelize);
 
-// Display list of all BookInstances.
 exports.getUsers = (req, res) => {
     User.findAll({
         raw: true
     }).then((rs) => {
-        res.status(200).send(rs);
+        res.status(200).json(rs||"{'error':'users not found'}");
     }).catch((e) => {
         console.log('Error: ', e);
         res.status(500);
     });
 }
 exports.getUser = (req, res) => {
-    const id = req.params.id;
+    const id = escape(req.params.id);
     User.findByPk(id, {
         raw: true
     }).then((rs) => {
-        res.status(200).send(rs);
+        console.log(rs)
+        res.status(200).json(rs||`{'error':'user ${id} not found'}`);
     }).catch((e) => {
         console.log('Error: ', e);
         res.status(500);
