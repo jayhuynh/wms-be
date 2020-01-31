aba112781fbc71128b031cbb728be7fe4f78b012
NhanVo <voasd123@gmail.com>
2020-01-31 21:32:11 +0700

    WMS-32 Update Access-Control-Allow-Origin

--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ const dotenv = require('dotenv').config();
 const PORT = process.env.PORT;
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+});
+
 app.get('/', (req, res) => res.send('INDEX'));
 
 app.use('/users', require('./routes/routers/user'));
