1e54ebc61a241a8c7f9712302260ac9fd8b1eb67
NhanVo <voasd123@gmail.com>
2020-01-31 21:45:25 +0700

    WMS-36 Delete a user by its id

--- src/routes/controllers/userController.js
+++ src/routes/controllers/userController.js
@@ -3,6 +3,10 @@ const {
     Sequelize
 } = require('../../db');
 
+const {
+    Op
+} = Sequelize
+
 const User = require('../../db/models/user')(db, Sequelize);
 
 exports.getUsers = (req, res) => {
@@ -15,10 +19,13 @@ exports.getUsers = (req, res) => {
             res.status(404).json(`{"error":"users not found"}`);
         }
     }).catch((e) => {
-        console.log('Error: ', e);
-        res.status(500).send();
+        console.log('Error: ', e.message);
+        res.status(500).send({
+            error: e.message
+        });
     });
 }
+
 exports.getUser = (req, res) => {
     const id = escape(req.params.id);
     User.findByPk(id, {
@@ -30,7 +37,40 @@ exports.getUser = (req, res) => {
             res.status(404).json(`{'error':'user ${id} not found'}`);
         }
     }).catch((e) => {
-        console.log('Error: ', e);
-        res.status(500).send();
+        console.log('Error: ', e.message);
+        res.status(500).send({
+            error: e.message
+        });
     });
 };
+
+exports.deleteUser = async (req, res) => {
+    const id = escape(req.params.id);
+
+    User.update({
+        deletedAt: Sequelize.fn('NOW')
+    }, {
+        where: {
+            id,
+            deletedAt: {
+                [Op.is]: null
+            }
+        }
+    }).then((rs) => {
+        //return [0] if there is no row updated(not existed id or deleted_at is not null)
+        if (rs[0] === 0) {
+            //not existed id or user with id is already deleted
+            return res.status(400).send({
+                error: "Invalid id. Either user is already deleted or it is not existed."
+            })
+        } else {
+            return res.send();
+        }
+
+    }).catch((e) => {
+        console.log('Error: ', e.message);
+        res.status(500).send({
+            error: e.message
+        });
+    })
+}
