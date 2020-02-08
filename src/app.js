a9ca7233b925471ae3980bfe4b1d4cbb1c461112
NhanVo <voasd123@gmail.com>
2020-02-08 13:31:58 +0700

    WMS-43 Set Allow methods header

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-
 const PORT = process.env.PORT;
-const app = express();
+const app = express();	const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    next();
-});
+const ALLOW_ORIGIN = process.env.ALLOW_ORIGIN;
+// Add headers
+app.use((req, res, next) => {	app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");	
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', ALLOW_ORIGIN);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    // Pass to next layer of middleware
+    next();	    next();
+});	});
 
 app.get('/', (req, res) => res.send('INDEX'));
 
